7bd36e718bde64a1e40a591b9fa0bb4c8cc20c28
Owen Parker <Oheadthaboss@gmail.com>
2023-12-04 18:07:14 -0500

    Added Cesspool Online

--- populateDoodleTable.js
+++ populateDoodleTable.js
@@ -28,6 +28,7 @@ const table = document.getElementById('doodle-table');
 const doodles = [
 	new Doodle('Cookie Clicker Gift Code Editor','<a href="https://orteil.dashnet.org/cookieclicker">Cookie Clicker</a> has a gift code feature. I made an editor for them','https://therealohead.github.io/cookie-clicker-gift-code-editor/'),
 	new Doodle('Matcher','A simple enough block-break puzzle game','https://therealohead.github.io/matcher/'),
+	new Doodle('Cesspool Online','A horrible social media site where everyone\'s anonymous','http://cesspoolonline.xyz/')
 	new Doodle('Conway\'s Game of Life','My implementation of Conway\'s Game of Life','https://therealohead.github.io/cgol/'),
 	new Doodle('Tax TD','An unfinished demo for a tower defense game about not paying taxes','https://therealohead.github.io/tax-td/'),
 	new Doodle('Neverending Legacy Mods','A collection of mods I made for Orteil\'s other game: <a href="https://orteil.dashnet.org/legacy">Neverending Legacy</a>','https://therealohead.github.io/neverending-legacy-mods'),
